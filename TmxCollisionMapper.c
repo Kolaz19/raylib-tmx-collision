44087cf03f942cd6f9eb73fe6b1a09eb4c50d204
Nikolas <etc.nikolas@gmail.com>
2023-03-11 19:12:47 +0100

    Extracted collision data

--- TmxCollisionMapper.c
+++ TmxCollisionMapper.c
@@ -1,15 +1,29 @@
 #include "TmxCollisionMapper.h"
 #include "include/tmx.h"
+#include <stdbool.h>
+
+
 
 void testMethodTmx(void) {
+int collisionCount = 0;
 tmx_resource_manager* rm = tmx_make_resource_manager();
 tmx_map* map = tmx_rcmgr_load(rm, "simpleMap.tmx");
 
-tmx_object* col1 = map->ts_head->tileset->tiles->collision;
-tmx_object* col2 = (map->ts_head->tileset->tiles+1)->collision;
+for (int i = 0; i < map->ts_head->tileset->tilecount; i++) {
+    if ((map->ts_head->tileset->tiles+i)->collision != NULL) collisionCount++;
+}
+
+
+tmx_object* colObjects[collisionCount];
+collisionCount = 0;
+for (int i = 0; i < map->tilecount; i++) {
+    if ((map->ts_head->tileset->tiles+i)->collision != NULL) {
+        colObjects[collisionCount] = (map->ts_head->tileset->tiles+i)->collision;
+        collisionCount++;
+    }
+}
 
 
 tmx_map_free(map);
 tmx_free_resource_manager(rm);
-int hey = 7;
 }
--- example/pixil-frame-0.tsx
+++ example/pixil-frame-0.tsx
@@ -1,14 +1,14 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="pixil-frame-0" tilewidth="16" tileheight="16" tilecount="2" columns="1">
- <image source="pixil-frame-0.png" width="16" height="32"/>
+<tileset version="1.9" tiledversion="1.9.2" name="pixil-frame-0" tilewidth="16" tileheight="16" tilecount="3" columns="1">
+ <image source="pixil-frame-0.png" width="16" height="48"/>
  <tile id="0">
   <objectgroup draworder="index" id="2">
    <object id="1" x="0.0078125" y="0.0131944" width="15.9833" height="15.9771"/>
   </objectgroup>
  </tile>
- <tile id="1">
-  <objectgroup draworder="index" id="3">
-   <object id="3" x="8.72727" y="0.727273" width="6.63636" height="6.36364"/>
+ <tile id="2">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.545455" y="5.27273" width="8.90909" height="10.3636"/>
   </objectgroup>
  </tile>
 </tileset>
